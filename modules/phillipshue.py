2ad4b1f2806d7d054bba121f82f35607ef37fea1
CodeZhifty <juulstausholmoscar@gmail.com>
2025-01-31 12:09:00 +0100

    Updated models

--- modules/phillipshue.py
+++ modules/phillipshue.py
@@ -1,9 +1,13 @@
+import os
+from dotenv import load_dotenv
 import urllib3
 urllib3.disable_warnings()
 from huesdk import Hue
 
+load_dotenv()
+
 bridge_ip = "192.168.1.210"
-username = "8uw4SmJNOFfYREWKSsQ245MnGYLu8Ox8tcDkYQE4"
+username = os.getenv("HUE_USERNAME")
 
 hue = Hue(bridge_ip=bridge_ip, username=username)
 
